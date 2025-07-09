fb1e3ee49d96935ae07164de94ebad4706b26750
River.Shi <nachuan.shi.quant@gamil.com>
2025-07-09 08:17:49 +0000

    Refactor header initialization in DataDumper class for improved readability

--- bybit_dump/dump.py
+++ bybit_dump/dump.py
@@ -87,28 +87,28 @@ class DataDumper:
         self._loop = asyncio.get_event_loop()
         self._chunk_size = chunk_size
         self._proxy = proxy
-        # self._headers = {
-        #     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        #     "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-        #     "cache-control": "no-cache",
-        #     "pragma": "no-cache",
-        #     "priority": "u=0, i",
-        #     "sec-ch-ua": '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
-        #     "sec-ch-ua-mobile": "?0",
-        #     "sec-ch-ua-platform": '"macOS"',
-        #     "sec-fetch-dest": "document",
-        #     "sec-fetch-mode": "navigate",
-        #     "sec-fetch-site": "none",
-        #     "sec-fetch-user": "?1",
-        #     "upgrade-insecure-requests": "1",
-        #     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-        # }
-        haader = Headers(
-            browser="chrome",
-            os="mac",
-            headers=True,
-        )
-        self._headers = haader.generate()
+        self._headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "priority": "u=0, i",
+            "sec-ch-ua": '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+        }
+        # haader = Headers(
+        #     browser="chrome",
+        #     os="mac",
+        #     headers=True,
+        # )
+        # self._headers = haader.generate()
 
         now = (
             datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1)
